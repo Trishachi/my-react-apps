05d2d70df08fb82c68d86afe734850884344041e
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-01-07 23:18:17 -0700

    Updates

---
+++ precache-manifest.9b829bcd0694b3d3f91cf1ddc4e54c68.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdbdbdfc65d7a11cb004a5afdddb9aa",
+    "url": "/my-react-apps/index.html"
+  },
+  {
+    "revision": "fa4bdae6533a5c29b4bc",
+    "url": "/my-react-apps/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "87fe0177dffaf54c51a0",
+    "url": "/my-react-apps/static/css/main.e7872eaf.chunk.css"
+  },
+  {
+    "revision": "fa4bdae6533a5c29b4bc",
+    "url": "/my-react-apps/static/js/2.f4ed6800.chunk.js"
+  },
+  {
+    "revision": "87fe0177dffaf54c51a0",
+    "url": "/my-react-apps/static/js/main.361be567.chunk.js"
+  },
+  {
+    "revision": "28874504ae915f848b69",
+    "url": "/my-react-apps/static/js/runtime-main.a971bd2e.js"
+  },
+  {
+    "revision": "37db4ad6d250e150f640c753c6cf08bf",
+    "url": "/my-react-apps/static/media/accounts.37db4ad6.svg"
+  },
+  {
+    "revision": "481f3486b21c8e9c98eb46152613e0b0",
+    "url": "/my-react-apps/static/media/cities.481f3486.svg"
+  },
+  {
+    "revision": "6ccfdef63ac4b78777fb0b8e0cdb5981",
+    "url": "/my-react-apps/static/media/link.6ccfdef6.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-react-apps/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "b165e1afd0619b2c50581b5ab788cbc5",
+    "url": "/my-react-apps/static/media/settings.b165e1af.svg"
+  },
+  {
+    "revision": "348ca21baa03ae96bd2579ba74209d64",
+    "url": "/my-react-apps/static/media/tictactoe.348ca21b.svg"
+  },
+  {
+    "revision": "c8ecb422d35086364b779fb3cc2be0c0",
+    "url": "/my-react-apps/static/media/welcome.c8ecb422.svg"
+  }
+]);
